66d58f57621ffe2f6589342c4ff49f81274d88bc
Morilence <1280659615@qq.com>
2020-03-17 18:27:26 +0800

    增加ObjectId替换，使$match支持_id匹配

--- modules/crud-router/index.js
+++ modules/crud-router/index.js
@@ -45,6 +45,22 @@ class CRUDRouter {
                     assert.ok(payload.pipeline!=undefined, "pipeline must be defined");
                     payload.pipeline = JSON.parse(payload.pipeline);
                     assert.equal(Object.prototype.toString.call(payload.pipeline), '[object Array]', "pipeline must be an array");
+                    // 将_id字符串转换为ObjectId
+                    let indexOfMatch = payload.pipeline.findIndex(opeItem => opeItem["$match"] != undefined);
+                    if (indexOfMatch != -1) {
+                        let $match = payload.pipeline[indexOfMatch]["$match"];
+                        if ($match._id != undefined) {
+                            if (typeof $match._id == "string") {
+                                payload.pipeline[indexOfMatch]["$match"]._id = ObjectId($match._id);
+                            } else if (typeof $match._id == "object") {
+                                if ($match._id["$in"] != undefined) {
+                                    payload.pipeline[indexOfMatch]["$match"]._id["$in"].forEach(idItem => {
+                                        idItem = ObjectId(idItem);
+                                    });
+                                }
+                            }
+                        }
+                    }
                     await this.db.aggregate(
                         this.collectionName,
                         payload.pipeline,
